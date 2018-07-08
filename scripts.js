b2025be3bd1facb8acfe377cf016b682f4e7fb9d
Eric Mellow <ericmellow@gmail.com>
2018-07-08 16:12:23 -0600

    Add functionality to change active state of tabs on click

---
+++ scripts.js
@@ -0,0 +1,22 @@
+var $tab1 = $('.tab1');
+var $tab2 = $('.tab2');
+var $tab3 = $('.tab3');
+var $tab4 = $('.tab4');
+
+$tab1.on('click', toggleActive)
+$tab2.on('click', toggleActive)
+$tab3.on('click', toggleActive)
+$tab4.on('click', toggleActive)
+
+function toggleActive(event) {
+  event.preventDefault();
+  const tab = $(this).attr('class').split(' ')[0]
+  toggleTab(tab);
+}
+
+function toggleTab(tab) {
+  const deactivateTab = $('.tab-active')
+  deactivateTab.toggleClass('tab-active')
+  const activateTab = $(`.${tab}`)
+  activateTab.toggleClass('tab-active')
+}
